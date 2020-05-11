8ea5ae198561c26907b725c1462d3ab261e1bb3f
Danila Martsinchyk <50248309+DanilaMartsinchyk@users.noreply.github.com>
2020-05-10 21:54:07 -0700

    Update Obstacles.java

--- FreedomFinders/src/Obstacles/Obstacles.java
+++ FreedomFinders/src/Obstacles/Obstacles.java
@@ -6,45 +6,61 @@ import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Obstacles extends Rectangle2D.Double {
-    private PImage image;
-    private int x;
-    private int y;
-    private int w;
-    private int h;
-    /**
-     * creates an Obstacles object
-     * @param i a PImage of the obstacle
-     * @param x x coordinate of the top left corner of the obstacle
-     * @param y y coordinate of the top left corner of the obstacle
-     * @param w width of the obstacle
-     * @param h height of the obstacle
-     */
-    public Obstacles(PImage i, int x, int y, int w, int h) {
-        super(x,y,30,35);
-        image = i;
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-    }
-    /**
-     * moves the obstacle
-     */
-    public void move() {
-        this.x -= 3;
-    }
-    /**
-     * returns the x value of the obstacle
-     */
-    public double getX() {
-        return x;
-    }
-    /**
-     * draws the obstacle
-     * @param drawer used to help draw the obstacle
-     */
-    public void draw(PApplet drawer) {
-        drawer.image(image, (int) x, (int) y, (int) width, (int) height);
-    }
-    
+	private PImage image;
+	private int x;
+	private int y;
+	private int w;
+	private int h;
+	private int health;
+	private int difficulty;
+
+	public Obstacles(PImage i, int x, int y, int w, int h) {
+		super(x, y, 30, 35);
+		image = i;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		health = 100;
+		difficulty = 0;
+	}
+
+	public void setDifficulty(int difficulty) {
+		this.difficulty = difficulty;
+	}
+
+	public void move() {
+		if (difficulty == 0) {
+			this.x -= 3;
+		}
+		if (difficulty == 1) {
+			this.x -= 3.5;
+		}
+		if (this.difficulty == 2) {
+			this.x -= 4;
+		}
+
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void subtractHealth(int health) {
+		this.health -= health;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void draw(PApplet drawer) {
+		if (health == 50) {
+			drawer.fill(255, 0, 0);
+			drawer.rect(x + 5, y - 10, 25, 10);
+		}
+		drawer.image(image, (int) x, (int) y, (int) width, (int) height);
+		drawer.fill(0, 0, 0);
+	}
+
 }
